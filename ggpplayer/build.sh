7fa4354da4f48c0b5f08c2696896c560b0cf1559
zhf <pkuzhf@pku.edu.cn>
2014-05-27 06:55:15 +0800

    update build.sh

--- ggpplayer/build.sh
+++ ggpplayer/build.sh
@@ -1,1 +1,1 @@
-g++ dependgraph.h dependgraph.cpp ggpplayer.cpp prover.h prover.cpp reader.h reader.cpp relation.h relation.cpp statemachine.h statemachine.cpp -std=c++11 -o2 -o ggp
+g++ -std=c++11 -o3 -o ggp dependgraph.h dependgraph.cpp ggpplayer.cpp prover.h prover.cpp reader.h reader.cpp relation.h relation.cpp statemachine.h statemachine.cpp
