0ccd9825b3bd0467fb74ffae7e87e07d75e74688
huangzhipeng <huangzhipeng@pku.edu.cn>
2014-05-30 11:06:02 +0800

    player done

--- ggpplayer/ggpplayer.cpp
+++ ggpplayer/ggpplayer.cpp
@@ -29,14 +29,18 @@ int main() {
     }
 	Relations rs;
 	r.getRelations(rs);
-	StateMachine machine(rs);
-	/*char buf[10000];
+	//StateMachine machine(rs);
+	char buf[10000];
 	cin.getline(buf, 10000);
-	int role;
-	role = Relation::symbol2code[string(buf)];*/
+	int role, runtime;
+	role = Relation::symbol2code[string(buf)];
+	cin.getline(buf, 10000);
+	runtime = atoi(buf);
+	MonteCarloPlayer Mplayer(rs, role);   // montecarlo player
 	
+
 	// check montecarlo player
-	MonteCarloPlayer Mplayer1(rs, 0);
+	/*MonteCarloPlayer Mplayer1(rs, 0);
 	MonteCarloPlayer Mplayer2(rs, 1);
 	while(!Mplayer1.is_terminal_){
 		Propositions moves;
@@ -46,40 +50,41 @@ int main() {
 		Mplayer2.goOneStep(moves);
 		cout<<"real moves:"<< moves[0].toString()<<endl;
 		cout<<"real moves:"<< moves[1].toString()<<endl;
-	}
-	//
+	}*/
+	
 
-	//for (int i = 0; i < machine.prover_.roles_.size(); ++i) {
-	//	if (machine.prover_.roles_[i].items_[0].head_ == role) {
-	//		role = i;
-	//		break;
-	//	}
-	//}
-	//cout << "ready" << endl;
-	//	
-	//cin.getline(buf, 10000);
-	//cout << machine.getRandomMove(role).items_[1].toString() << endl;
-	//
-	//while (true) {				
-	//	cin.getline(buf, 10000);	
-	//	Reader move_reader;
-	//	move_reader.file_head_ = buf;		
-	//	Propositions joint_move;
-	//	move_reader.getMoves(joint_move);
-	//	for (int i = 0; i < joint_move.size(); ++i) {
-	//		Relation does;
-	//		does.head_ = r_does;						
-	//		does.items_.push_back(machine.prover_.roles_[i].toRelation().items_[0]);
-	//		does.items_.push_back(joint_move[i].toRelation());
-	//		joint_move[i] = does.toProposition();
-	//	}		
-	//	machine.goOneStep(joint_move);
-	//	if (machine.is_terminal_) {
-	//		//cout << "Terminal." << endl;
-	//		break;
-	//	}
-	//	Proposition move = machine.getRandomMove(role);
-	//	cout << move.items_[1].toString() << endl;
-	//}
+	for (int i = 0; i < Mplayer.stateMachine_.prover_.roles_.size(); ++i) {
+		if (Mplayer.stateMachine_.prover_.roles_[i].items_[0].head_ == role) {
+			Mplayer.roleNum_ = i;
+			role = i;
+			break;
+		}
+	}
+	cout << "ready" << endl;
+		
+	cin.getline(buf, 10000);
+	cout << Mplayer.stateMachineSelectMove(runtime).toString() << endl;
+	
+	while (true) {				
+		cin.getline(buf, 10000);	
+		Reader move_reader;
+		move_reader.file_head_ = buf;		
+		Propositions joint_move;
+		move_reader.getMoves(joint_move);
+		for (int i = 0; i < joint_move.size(); ++i) {
+			Relation does;
+			does.head_ = r_does;						
+			does.items_.push_back(Mplayer.stateMachine_.prover_.roles_[i].toRelation().items_[0]);
+			does.items_.push_back(joint_move[i].toRelation());
+			joint_move[i] = does.toProposition();
+		}		
+		Mplayer.goOneStep(joint_move);
+		if (Mplayer.is_terminal_) {
+			//cout << "Terminal." << endl;
+			break;
+		}
+		Proposition move = Mplayer.stateMachineSelectMove(runtime);
+		cout << move.items_[1].toString() << endl;
+	}
 	return 0;
 }
