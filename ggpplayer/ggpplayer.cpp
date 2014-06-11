040b6a38686a2188abcc4861259ef29876311027
zhf <pkuzhf@126.com>
2014-06-11 17:22:14 +0800

    fix bug

--- ggpplayer/ggpplayer.cpp
+++ ggpplayer/ggpplayer.cpp
@@ -59,7 +59,7 @@ int main() {
 	cin.getline(buf, 10000);
 	int playclock = (atoi(buf) - 2) * CLOCKS_PER_SEC;
 
-	MonteCarloPlayer player(rs, string(buf));   // montecarlo player
+	MonteCarloPlayer player(rs, role);   // montecarlo player
 	// check montecarlo player
 	/*MonteCarloPlayer Mplayer1(rs, 0);
 	MonteCarloPlayer Mplayer2(rs, 1);
