8b3bcf5a272c079a087589d7bbe7af2d21af47c5
zhf <pkuzhf@126.com>
2014-06-23 17:05:26 +0800

    debug

--- ggpplayer/ggpplayer.cpp
+++ ggpplayer/ggpplayer.cpp
@@ -98,11 +98,11 @@ void run_server() {
 			state_reader.getPropositions(state);			
 			player.updateTree(state, string(semi + 1));
 			ostringstream o;
-			o << "(" << (int)player.root_.getScore() << "/" << player.root_.attemps_ << ") ";
+			o << "(" << player.root_.points_ / (player.root_.attemps_ + 1) << "/" << player.root_.attemps_ << ") ";
 			for (int i = 0; i < player.root_.sons_.size(); ++i) {
 				o << "{ ";
 				for (int j = 0; j < player.root_.sons_[i].size(); j++) {
-					o << "<" << (int)player.root_.sons_[i][j].getScore() << "/" << player.root_.sons_[i][j].attemps_ << "> ";
+					o << "<" << player.root_.sons_[i][j].points_ / (player.root_.sons_[i][j].attemps_ + 1) << "/" << player.root_.sons_[i][j].attemps_ << "> ";
 				}
 				o << "} ";
 			}
