5150a1f131854562270d02771dcf74aa2066aca1
zhf <pkuzhf@126.com>
2014-06-25 15:20:23 +0800

    debug

--- ggpplayer/montecarloplayer.cpp
+++ ggpplayer/montecarloplayer.cpp
@@ -19,10 +19,10 @@ void MonteCarloPlayer::updateTree(int code, Propositions state, string tree) {
 		return;
 	}
 	Node * node = nodes_[code];
-	//cerr << Client::message("debug", "updateTree");
-	//cerr << Client::message("debug", node->toString());
-	//state_machine_.setState(state);
-	//cerr << Client::message("debug", Proposition::propsToStr(state_machine_.getLegalMoves(role_num_)));
+	cerr << Client::message("debug", "updateTree");
+	cerr << Client::message("debug", node->toString());
+	state_machine_.setState(state);
+	cerr << Client::message("debug", Proposition::propsToStr(state_machine_.getLegalMoves(role_num_)));
 	long long old_points = node->points_;
 	long long old_attemps = node->attemps_;
 	updateNode(node, tree);
