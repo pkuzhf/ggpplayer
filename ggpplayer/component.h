a4ee319ac4c17e85e36a27cf397a890530b8a7c4
zhf <pkuzhf@126.com>
2014-07-17 22:13:40 +0800

    add propnet head file

---
+++ ggpplayer/component.h
@@ -0,0 +1,19 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <vector>
+
+const int c_and = 0;
+const int c_or = 1;
+const int c_not = 2;
+const int c_transition = 3;
+
+class Component {
+	int type_;
+	vector<Component *> inputs_;
+	vector<Component *> outputs_;
+	int value_;
+	int trues_;
+};
+
+#endif
---
+++ ggpplayer/propnet.h
@@ -0,0 +1,12 @@
+#ifndef PROPNET_H
+#define PROPNET_H
+
+#include "component.h"
+
+class Propnet {
+	vector<Component *> components_;
+	
+
+};
+
+#endif
