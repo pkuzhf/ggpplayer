63c37a77a6262e8893bc39e575c9caeb04e080f7
zhf <pkuzhf@126.com>
2014-07-19 17:15:41 +0800

    tmp

--- ggpplayer/component.h
+++ ggpplayer/component.h
@@ -9,11 +9,14 @@ const int c_not = 2;
 const int c_transition = 3;
 
 class Component {
+public:
 	int type_;
 	vector<Component *> inputs_;
 	vector<Component *> outputs_;
 	int value_;
 	int trues_;
+
+	Component(int type, );
 };
 
 #endif
--- ggpplayer/propnet.h
+++ ggpplayer/propnet.h
@@ -1,12 +1,16 @@
 #ifndef PROPNET_H
 #define PROPNET_H
 
+#include <vector>
+
 #include "component.h"
+#include "relation.h"
 
 class Propnet {
+public:
 	vector<Component *> components_;
 	
-
+	void init(Relations ps);
 };
 
 #endif
